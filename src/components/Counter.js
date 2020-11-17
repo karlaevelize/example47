2cd0a06133ef2ae52638836d5ca2b27c0c01f9f5
karlaevelize <k.evelize@gmail.com>
2020-11-17 14:44:27 +0100

    added a reset button to Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,10 +8,15 @@ export default function Counter (){
         setCount(count + 1)
     }
 
+    const reset = () => {
+        setCount(count - count) //how to reset the state
+    }
+
     console.log(count)
 
     return <div>
        <h2> number: {count}</h2>
+       <button onClick={reset}>reset</button>
        <button onClick={counter}>+1</button>
     </div>
 }
